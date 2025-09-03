eb972b2604039908e21abf59ff25a43f1020c2c9
Anirudh-Bhavanasi <bhavanasi3111@mydusd.org>
2025-09-03 14:07:45 -0700

    Anirudh commit

--- Classroom.java
+++ Classroom.java
@@ -152,6 +152,10 @@ public class Classroom extends World
         KilgoreTrout kilgoretrout = new KilgoreTrout();
         addObject(kilgoretrout,2,3);
         kilgoretrout.assignSeat();
+        
+        RuBhavanasi rubhavanasi = new RuBhavanasi();
+        addObject(rubhavanasi,6,7);
+        rubhavanasi.assignSeat();
   
     }
     
