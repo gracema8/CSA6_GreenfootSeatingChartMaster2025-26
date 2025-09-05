d02375de0b8778177cf14c7ce02d82f8af3f453e
SanjayKrishnan15 <krishnan7958@mydusd.org>
2025-09-05 13:39:19 -0700

    pl

--- Classroom.java
+++ Classroom.java
@@ -152,6 +152,9 @@ public class Classroom extends World
         KilgoreTrout kilgoretrout = new KilgoreTrout();
         addObject(kilgoretrout,2,3);
         kilgoretrout.assignSeat();
+        KrishnanSanjay krishnansanjay = new KrishnanSanjay();
+        addObject(krishnansanjay,8,3);
+        krishnansanjay.assignSeat();
   
     }
     
